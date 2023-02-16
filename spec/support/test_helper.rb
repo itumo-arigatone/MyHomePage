b0298478863f145050a3a95981641dc3ba3ec8eb
itumo-arigatone <soganokasegi0825@gmail.com>
2023-02-16 22:40:08 +0900

    create rspec helper file

---
+++ spec/support/test_helper.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+module TestHelper
+
+  include ApplicationHelper
+
+  # def is_logged_in?
+  #   !session[:user_id].nil?
+  # end
+
+  # def log_in_as(user)
+  #   session[:user_id] = user.id
+  # end
+
+end
