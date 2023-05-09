6c3877d1c3eb05c84df216adc1fe18b99ea90ae9
itumo-arigatone <soganokasegi0825@gmail.com>
2023-05-10 00:19:29 +0900

    enable account activation from mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # TODO: ユーザ登録が完了したらメールを送信する
-      log_in @user
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
